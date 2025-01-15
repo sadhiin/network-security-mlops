c5d69537910c70e6c5d82285f64d82bebc4e2632
Shanjidul Islam Sadhin <sanjidulislamsadhin1994@gmail.com>
2025-01-15 18:44:17 +0000

    added the files

--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
-from setup import setup, find_packages
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    """This function installs the required packages for the project
+
+    Returns:
+        List[str]: _description_
+    """
+    try:
+        requried_list = []
+        with open('requirements.txt') as f:
+            lines =  f.readlines()
+            for line in lines:
+                requriments = line.split()
+                if requriments and requriments != "-e .":
+                    requried_list.append(requriments[0])
+       
+    except Exception as e:
+        print(f"Error: {e}")
+        return requried_list
+    
+    return requried_list
+
+# print(get_requirements())
+
+setup(
+    name = "networksecurity",
+    version="0.0.1",
+    author= "sadhiin",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
