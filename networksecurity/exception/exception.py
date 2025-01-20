21540234f1879e71ff7b4134a0cd8543075f6b92
sadhiin <sanjidulislamsadhin1994@gmail.com>
2025-01-20 22:20:34 +0600

    Refactor exception handling and logging: updated error details handling in NetworkSecurityException, changed logger import structure, and corrected log file path. Removed unnecessary compiled Python files.

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,23 +1,23 @@
 import sys
-from networksecurity.logging.logger import logging
+from networksecurity.logging import logger
 
 class NetworkSecurityException(Exception):
-    def __init__(self, message, erro_detials:sys):
+    def __init__(self, message, error_details):
         self.message = message
-        _, _, exc_tb = erro_detials.exc_info()
+        _, _, exc_tb = error_details  # error_details is now the tuple returned by sys.exc_info()
         self.line_number = exc_tb.tb_lineno
         self.file_name = exc_tb.tb_frame.f_code.co_filename
         super().__init__(self.message)
-        
+
     def __str__(self):
-        return f"Error occured in python script name {self.file_name} at line number {self.line_number} with error message {self.message}"
-    
-    
+        return f"Error occurred in python script name [{self.file_name}] at line number [{self.line_number}] with error message [{self.message}]"
+
+
 if __name__=="__main__":
     try:
-        logging.info("This is a test log and enter the try block")
-        a = 1 /0
-        print("This will print if there is no exception")
+        logger.logging.info("This is a test log and enter the try block")
+        a = 1 / 0
+        logger.logging.info("This will print if there is no exception")
     except Exception as e:
-        logging.error(f"Error: {e}")
+        logger.logging.error(f"Error: {e}")
         raise NetworkSecurityException("This is a test exception", sys.exc_info())
