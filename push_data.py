990bda894781befa96f3e2ce2e1856660f38a4ff
Shanjidul Islam Sadhin <sanjidulislamsadhin1994@gmail.com>
2025-01-20 15:45:31 +0000

    Update the code

---
+++ push_data.py
@@ -0,0 +1,18 @@
+
+import os
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
+import certifi
+ca = certifi.where()
+uri = os.getenv('MONGODB_URI')
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
